98cf2a15f32a00b12b2d17c2ccd79f7026af2f84
AmanBhashkar <aman.bhashkar@genproresearch.com>
2023-08-11 01:16:10 +0530

    first chapter done

--- hello_test.go
+++ hello_test.go
@@ -3,9 +3,26 @@ package main
 import "testing"
 
 func TestHello(t *testing.T) {
-	got := Hello("aman")
-	want := "Hello aman"
+	t.Run("saying hello to people", func(t *testing.T) {
+		got := Hello("aman", "English")
+		want := "Hello, aman"
+		assertCorrectMessage(t, got, want)
+	})
+	t.Run("empty string default to 'world'", func(t *testing.T) {
+		got := Hello("", "English")
+		want := "Hello, World"
+		assertCorrectMessage(t, got, want)
 
+	})
+
+	t.Run("in Spanish", func(t *testing.T) {
+		got := Hello("Elodie", "Spanish")
+		want := "Hola, Elodie"
+		assertCorrectMessage(t, got, want)
+	})
+}
+func assertCorrectMessage(t testing.TB, got, want string) {
+	t.Helper()
 	if got != want {
 		t.Errorf("got %q want %q", got, want)
 	}
--- hello_world.go
+++ hello_world.go
@@ -1,10 +1,35 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
-func Hello(aman string) string {
-	return "Hello " + aman
+const (
+	french  = "French"
+	spanish = "Spanish"
+
+	english_greet = "Hello, "
+	french_greet  = "Bonjour, "
+	spanish_greet = "Hola, "
+)
+
+func Hello(name string, language string) string {
+	if name == "" {
+		name = "World"
+	}
+	return greetingPrefix(language) + name
+}
+func greetingPrefix(language string) (prefix string) {
+	switch language {
+	case french:
+		prefix = french_greet
+	case spanish:
+		prefix = spanish_greet
+	default:
+		prefix = english_greet
+	}
+	return
 }
 func main() {
-	fmt.Print(Hello("aman"))
+	fmt.Print(Hello("aman", "English"))
 }
